8113fed995bc12b55eba1b80ee61369af30fe01b
android <trevd1234@gmail.com>
2014-03-29 15:03:30 +0000

    Update main make file

--- Android.mk
+++ Android.mk
@@ -5,8 +5,11 @@ LOCAL_VERSION := 2.23.52.0.2
 BUILD_LIBBFD := $(LOCAL_PATH)/android/libbfd.mk
 BUILD_LIBOPCODES := $(LOCAL_PATH)/android/libopcodes.mk
 BUILD_LIBIBERTY := $(LOCAL_PATH)/android/libiberty.mk
-BUILD_LIBIBERTY := $(LOCAL_PATH)/android/gas.mk
+BUILD_GAS := $(LOCAL_PATH)/android/gas.mk
+BUILD_LD := $(LOCAL_PATH)/android/ld.mk
 
 include $(BUILD_LIBBFD)
 include $(BUILD_LIBOPCODES)
 include $(BUILD_LIBIBERTY)
+include $(BUILD_GAS)
+include $(BUILD_LD)
